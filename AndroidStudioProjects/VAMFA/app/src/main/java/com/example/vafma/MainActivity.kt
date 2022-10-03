710e57754c5c350bd6ceb9018d18ac53be97cbaa
Mousa Touré <66438522+MousaThe14@users.noreply.github.com>
2022-10-02 23:08:54 -0400

    Creating Web View
    Attempted and failed to create a webview page that can pull from the VAFMA but I can't get webview to work

--- AndroidStudioProjects/VAMFA/app/src/main/java/com/example/vafma/MainActivity.kt
+++ AndroidStudioProjects/VAMFA/app/src/main/java/com/example/vafma/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.vafma
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://vafma.org/virginia-markets/")
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportZoom(true)
+    }
+
+    // if you press Back button this code will work
+    override fun onBackPressed() {
+        // if your webview can go back it will go back
+        if (webView.canGoBack())
+            webView.goBack()
+        // if your webview cannot go back
+        // it will exit the application
+        else
+            super.onBackPressed()
+
     }
 }
