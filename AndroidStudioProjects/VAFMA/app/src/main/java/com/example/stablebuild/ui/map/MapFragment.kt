2019c7ea9440515f172c5a14656d959808b6bbe7
Mousa Touré <66438522+MousaThe14@users.noreply.github.com>
2022-11-29 11:58:33 -0500

    Uploading VAFMA
    VAFMA is the current and most updated stable build of the project

---
+++ AndroidStudioProjects/VAFMA/app/src/main/java/com/example/stablebuild/ui/map/MapFragment.kt
@@ -0,0 +1,78 @@
+//Fragment solution sourced from Stack Overflow:
+// https://stackoverflow.com/questions/65401994/webview-in-fragment-doesnt-load-any-webpage-kotlin-android
+
+package com.example.vafma.ui.map
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.databinding.DataBindingUtil
+import com.example.vafma.R
+import android.webkit.WebSettings
+import com.example.vafma.R.layout
+import com.example.vafma.databinding.FragmentMapBinding
+
+class MapFragment : Fragment() {
+
+    // private var _binding: FragmentMapBinding? = null
+
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+  //  private val binding get() = _binding!!
+  //  private lateinit var _binding: FragmentMapBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    //
+    //mappage.webViewClient = object : WebViewClient() {
+        override fun onViewCreated(view: View, savedInstanceStance: Bundle?){
+        val mappage: WebView = view.findViewById(R.id.map)
+            mappage.webViewClient = object: WebViewClient(){
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: String
+
+        ): Boolean {
+                    if (view != null) {
+                        view.loadUrl(request)
+                    }
+            return true
+        }
+    }
+        mappage.loadUrl("https://vafma.org/virginia-markets/")
+        mappage.settings.javaScriptEnabled = true
+        mappage.settings.allowContentAccess = true
+        mappage.settings.domStorageEnabled = true
+        mappage.settings.useWideViewPort = true
+        //_binding = FragmentMapBinding.inflate(inflater, container, false)
+     //   val binding: FragmentMapBinding = DataBindingUtil.setContentView(this, R.layout.fragment_home)
+
+        //binding.map.loadUrl("http://www.example.com")
+
+
+        //
+       //  val root: View = binding.root
+
+
+       // return root
+  //  }
+
+   // override fun onDestroyView() {
+      //  super.onDestroyView()
+      //  _binding = null
+    }
+}
---
+++ AndroidStudioProjects/VAFMA/app/src/main/java/com/example/stablebuild/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,47 @@
+package com.example.vafma.ui.notifications
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.vafma.R
+import com.example.vafma.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
+    }
+
+    //
+
+    override fun onViewCreated(view: View, savedInstanceStance: Bundle?){
+        val aboutpage: WebView = view.findViewById(R.id.about)
+        aboutpage.webViewClient = object: WebViewClient(){
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: String
+
+            ): Boolean {
+                if (view != null) {
+                    view.loadUrl(request)
+                }
+                return true
+            }
+        }
+        aboutpage.loadUrl("https://vafma.org/about/")
+        aboutpage.settings.javaScriptEnabled = true
+        aboutpage.settings.allowContentAccess = true
+        aboutpage.settings.domStorageEnabled = true
+        aboutpage.settings.useWideViewPort = true
+    }
+}
